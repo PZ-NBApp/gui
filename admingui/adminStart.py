0bf902d0f7784001bcc91bfdc6e433472d2e0f64
JPogodzinski <pogodzinskij@gmail.com>
2020-05-12 14:36:35 +0200

    admin buttons CLICKing

--- admingui/adminStart.py
+++ admingui/adminStart.py
@@ -2,6 +2,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 
 class Ui_adminStart(object):
+    def addPlayer(self):
+        print("player")
+
+    def addTeam(self):
+        print("Team")
+
+    def createGame(self):
+        print("game")
+
     def setupUi(self, adminStart):
         adminStart.setObjectName("adminStart")
         adminStart.resize(640, 480)
@@ -15,17 +24,20 @@ class Ui_adminStart(object):
         self.verticalLayout.addItem(spacerItem)
         self.addPlayerButton = QtWidgets.QPushButton(self.centralwidget)
         self.addPlayerButton.setObjectName("addPlayerButton")
+        self.addPlayerButton.clicked.connect(self.addPlayer)
         self.verticalLayout.addWidget(self.addPlayerButton)
         spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem1)
         self.addTeamButton = QtWidgets.QPushButton(self.centralwidget)
         self.addTeamButton.setObjectName("addTeamButton")
+        self.addTeamButton.clicked.connect(self.addTeam)
         self.verticalLayout.addWidget(self.addTeamButton)
         spacerItem2 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem2)
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.verticalLayout.addWidget(self.pushButton_3)
+        self.createGameButton = QtWidgets.QPushButton(self.centralwidget)
+        self.createGameButton.setObjectName("createGameButton")
+        self.createGameButton.clicked.connect(self.createGame)
+        self.verticalLayout.addWidget(self.createGameButton)
         spacerItem3 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem3)
         self.verticalLayout_2.addLayout(self.verticalLayout)
@@ -39,4 +51,15 @@ class Ui_adminStart(object):
         adminStart.setWindowTitle(_translate("adminStart", "Admin"))
         self.addPlayerButton.setText(_translate("adminStart", "Add Player"))
         self.addTeamButton.setText(_translate("adminStart", "Add Team"))
-        self.pushButton_3.setText(_translate("adminStart", "Create game"))
+        self.createGameButton.setText(_translate("adminStart", "Create game"))
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    adminStart = QtWidgets.QMainWindow()
+    ui = Ui_adminStart()
+    ui.setupUi(adminStart)
+    adminStart.show()
+    sys.exit(app.exec_())
